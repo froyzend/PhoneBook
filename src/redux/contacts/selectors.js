8c1a5a217215792f94e619d7f2092e43ca82e2ab
Froyzend <froyzend1993@gmail.com>
2024-12-12 12:06:41 +0100

    add

--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -4,16 +4,6 @@ export const selectAllContacts = (state) => state.contacts.items;
 // Селектор для получения фильтра по имени
 export const selectNameFilter = (state) => state.filters.name;
 
-// Селектор для получения отфильтрованных контактов
-export const selectFilteredContacts = (state) => {
-  const filter = state.filters.name.toLowerCase(); // нормализация фильтра
-  return state.contacts.items.filter(
-    (contact) =>
-      contact.name.toLowerCase().includes(filter) ||
-      contact.phone.includes(filter) // фильтрация по имени или телефону
-  );
-};
-
 // Селектор для получения статуса загрузки контактов
 export const selectContactsLoading = (state) => state.contacts.loading;
 
